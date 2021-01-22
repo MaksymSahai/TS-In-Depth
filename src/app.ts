fddcfc02f847900d62b1ef688bfa29e8d1da07e9
Maksym Sahai <maxym.sagay@gmail.com>
2021-01-22 13:23:23 +0200

    task 2.2

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ enum Category { JavaScript, Css, Html, TypeScript, Angular }
 
 
 function getAllBooks() {
-    const books = [
+    const books = <const>[
         { id: 1, category: Category.JavaScript, title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true },
         { id: 2, category: Category.JavaScript, title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false },
         { id: 3, category: Category.Css, title: 'CSS Secrets', author: 'Lea Verou', available: true },
@@ -44,7 +44,7 @@ function getBookAuthorByIndex(index: number): [string, string] {
 }
 
 function calcTotalPages(): bigint {
-    const data = [{ lib: 'libName1', books: 1_000_000_000, avgPagesPerBook: 250 },
+    const data = <const>[{ lib: 'libName1', books: 1_000_000_000, avgPagesPerBook: 250 },
     { lib: 'libName2', books: 5_000_000_000, avgPagesPerBook: 300 },
     { lib: 'libName3', books: 3_000_000_000, avgPagesPerBook: 280 }];
 
